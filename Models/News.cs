ee8f8092b1f642dc5755f023f0028622a8adae5e
ivanpaoloni@gmail.com <ivanpaoloni@gmail.com>
2023-06-04 16:31:28 -0300

    Add project files.

---
+++ Models/News.cs
@@ -0,0 +1,14 @@
+﻿namespace NewsBlazor.Models
+{
+    public class News
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        public string author { get; set; }
+        public string category { get; set; }
+        public DateTime publicationDate { get; set; }
+        public string imageUrl { get; set; }
+        public string content { get; set; }
+    }
+}
